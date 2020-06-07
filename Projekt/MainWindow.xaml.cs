62e5277e331f3365d03258927a2e178a817e455b
kinga.starzec96@gmail.com <tomasz.ptak@sklepopon.com>
2020-06-07 14:48:45 +0200

    Dodaj pliki projektów.

---
+++ Projekt/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void startGry_Click(object sender, RoutedEventArgs e)
+        {
+            Kolor.Fill = new SolidColorBrush(Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+        }
+    }
+}
